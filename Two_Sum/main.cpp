5c12323a9f1a32ed356cf1b8e288bd64d9700513
nopparuj917 <nopparuj.ta@gmail.com>
2023-02-17 22:53:29 +0700

    main

--- Two_Sum/main.cpp
+++ Two_Sum/main.cpp
@@ -1,9 +1,13 @@
-#include <bits/stdc++.h>
 #include "two_sum.h"
+#include <bits/stdc++.h>
 using namespace std;
 int main() {
-  vector<int> v = {1, 2, 3};
-
-  vector<int> r = twoSum(v, 3);
+  int m, t;
+  cin >> m >> t;
+  vector<int> v(m);
+  for (int i = 0; i < m; ++i) {
+    cin >> v[i];
+  }
+  vector<int> r = twoSum(v, t);
   cout << r[0] << " " << r[1] << endl;
 }
